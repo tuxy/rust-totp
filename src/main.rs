337a41f2c533710ff7dd78e299fc08fd4ce6f630
tuxy <lastpass7565@gmail.com>
2024-06-10 01:18:50 +1000

    Added settings.toml and cleaned up code

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,18 @@ use arboard::Clipboard;
 use clap::Parser;
 
 use std:: {
-    collections::HashMap, fs::{self, File}, io, sync::{atomic::{AtomicBool, Ordering}, Arc}, thread, time::{Duration, SystemTime, UNIX_EPOCH}
+    collections::HashMap, fs, io, sync::{atomic::{AtomicBool, Ordering}, Arc}, thread, time::{Duration, SystemTime, UNIX_EPOCH}
 };
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     name: String
+}
+
+#[derive(Deserialize, Debug)]
+struct Settings {
+    enable_clipboard: bool,
 }
 
 // Item struct to deserialize TOML
@@ -40,7 +45,13 @@ fn main() -> io::Result<()> {
         r.store(false, Ordering::SeqCst);
     }).expect("Cannot set Ctrl-C handler.");
 
+    // Load keys (name & secret) from HashMap
     let keys = &load_keys()["key"];
+
+    // Load settings into settings
+    let settings = load_settings();
+
+    let _test = load_settings();
     // Decodes secrets and prints out code + time left
     // Variable to (very shoddily) check if code has been found 
     let mut any: bool = false;
@@ -59,10 +70,13 @@ fn main() -> io::Result<()> {
                         panic!("Failed to generate codes from secret. Check if codes are valid.");
                     },
                 };
-                // Sets Clipboard to code
-                clipboard.set_text(code.to_string()).unwrap();
-        
-                thread::sleep(Duration::from_millis(500));
+                // Checks if clipboard is enabled
+                if settings.enable_clipboard {
+                    clipboard.set_text(code.to_string()).unwrap();
+                }
+                // Sleep to prevent timer from moving too quickly
+                // TODO: Perfectly matches presented time for TOTP > Sometimes moves too quickly
+                thread::sleep(Duration::from_millis(250));
                 pb.set_position(time_left(i.time));
                 any = true;
             }
@@ -98,17 +112,27 @@ fn load_keys() -> HashMap<String, Vec<Item>> {
 
     fs::create_dir_all(&app_dirs.config_dir).expect("Could not create config directory.");
 
-    let _file = if config_file_path.exists() {
-        File::open(&config_file_path).expect("Could not open config")
-    } else {
-        File::create(&config_file_path).expect("Could not create config")
-    };
-
     // TODO: Use a more reliable method to load TOML file directly into a TOML, without converting to string and back. Any minor error in TOML or string would cause program to flip out
     let items_string: String = fs::read_to_string(config_file_path.into_os_string())
         .expect("Could not load toml. Check format?");
-
-    let items_table: HashMap<String, Vec<Item>> = from_str(&items_string).expect("Could not load TOML Secrets. Please check formatting");
+    let items_table: HashMap<String, Vec<Item>> = from_str(&items_string)
+        .expect("Could not load TOML Secrets. Please check formatting");
     
+    items_table
+}
+
+// Loads settings into struct Settings. May be a more idiomatic way to do this without a seperate function.
+fn load_settings() -> Settings {
+    
+    let app_dirs = AppDirs::new(Some("rstotp"), true).unwrap();
+    let config_file_path = app_dirs.config_dir.join("settings.toml");
+
+    fs::create_dir_all(&app_dirs.config_dir).expect("Could not create config directory.");
+
+    let items_string = fs::read_to_string(config_file_path.into_os_string())
+        .expect("Could not load settings.toml. Check if file exists.");
+    let items_table: Settings = from_str(&items_string)
+        .expect("Could not load settings.toml. Check format?");
+
     items_table
 }
